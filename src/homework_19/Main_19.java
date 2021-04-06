786d8a9d3e51d7975d8cd846439b8fc59751c087
AnastasiiaKudryk <nastia.kudrik@gmail.com>
2021-04-06 11:40:59 +0300

    homework19 new

--- src/homework_19/Main_19.java
+++ src/homework_19/Main_19.java
@@ -1,8 +1,7 @@
 package homework_19;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
 
 public class Main_19 {
     public static void main(String[] args) {
@@ -18,16 +17,18 @@ public class Main_19 {
 
         list.forEach(person -> {
             Optional<Person> optionalPerson = Optional.ofNullable(person);
-            if(optionalPerson.isPresent())
-                System.out.println(optionalPerson.map(p -> p.getName()).orElse(defaultName));
+            optionalPerson.ifPresent(p ->
+                System.out.println(Optional.ofNullable(p.getName()).orElse(defaultName)));
         });
 
         List<Person> newPeople = new ArrayList<>();
 
         list.forEach(person -> {
             Optional<Person> optionalPerson = Optional.ofNullable(person);
-            if(optionalPerson.isPresent())
-                newPeople.add(new Person(optionalPerson.ofNullable(person.getName()).orElseGet(() -> defaultName), person.getAge()));
+            optionalPerson.ifPresent(p ->{
+                p.setName(Optional.ofNullable(p.getName()).orElse(defaultName));
+                newPeople.add(p);
+        });
         });
 
         newPeople.forEach(person -> System.out.println(person.getName()));
